243e32d0052cf7d7a940e6a774c4e17b097847ae
Jean Carlo Alvarado <105259214+ByJeanCa@users.noreply.github.com>
2025-04-09 12:38:21 -0600

    feature: The script now wait for the db test and db

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-until nc -z db 5432; do
-    echo "Esperando a PostgreSQL..."
+set -e
+
+# Default to 'prod' if ENV is not set
+ENV=${ENV:-prod}
+
+if [ "$ENV" = "test" ]; then
+  echo "Entorno: TEST. Esperando a db-test:5433..."
+  until nc -z db-test 5433; do
+    echo "Esperando a db-test en el puerto 5433..."
     sleep 2
-done
+  done
+else
+  echo "Entorno: PROD. Esperando a db:5432..."
+  until nc -z db 5432; do
+    echo "Esperando a db en el puerto 5432..."
+    sleep 2
+  done
+fi
+
+echo "Base de datos disponible. Continuando..."
 exec "$@"
