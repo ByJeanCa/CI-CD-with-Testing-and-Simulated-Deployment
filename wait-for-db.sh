5649e79f9017f929eeede885cd00a82d8eca7ed0
Jean Carlo Alvarado <105259214+ByJeanCa@users.noreply.github.com>
2025-04-09 12:49:14 -0600

    fix: Script didn't wait db was ready

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 
-# Default to 'prod' if ENV is not set
 ENV=${ENV:-prod}
 
 if [ "$ENV" = "test" ]; then
   echo "Entorno: TEST. Esperando a db-test:5433..."
-  until nc -z db-test 5433; do
-    echo "Esperando a db-test en el puerto 5433..."
+  until pg_isready -h db-test -p 5433 > /dev/null 2>&1; do
+    echo "Esperando a db-test..."
     sleep 2
   done
 else
   echo "Entorno: PROD. Esperando a db:5432..."
-  until nc -z db 5432; do
-    echo "Esperando a db en el puerto 5432..."
+  until pg_isready -h db -p 5432 > /dev/null 2>&1; do
+    echo "Esperando a db..."
     sleep 2
   done
 fi
