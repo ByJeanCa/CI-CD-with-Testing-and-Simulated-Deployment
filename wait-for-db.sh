5d56c9f685fa7e11a4406713314a55cc9cdbe4cc
byjeanca <jo7624822@gmail.com>
2025-04-10 07:56:55 -0600

    fix: The script now wait for the correct db-test port

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-until nc -z db 5432; do
-    echo "Esperando a PostgreSQL..."
+set -e
+
+ENV=${ENV:-prod}
+
+if [ "$ENV" = "test" ]; then
+  echo "Entorno: TEST. Esperando a db-test:5432..."
+  until nc -z db-test 5432; do
+    echo "Esperando a db-test en el puerto 5432..."
     sleep 2
-done
+  done
+else
+  echo "Entorno: PROD. Esperando a db:5432..."
+  until nc -z db 5432; do
+    echo "Esperando a db en el puerto 5432..."
+    sleep 2
+  done
+fi
+
+echo "Base de datos disponible. Continuando..."
 exec "$@"
